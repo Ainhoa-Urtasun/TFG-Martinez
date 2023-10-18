8518fa6af82b85e016b2841b3f30214515456d50
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-18 14:14:42 +0200

    Update TFG-Martinez.py

--- TFG-Martinez.py
+++ TFG-Martinez.py
@@ -9,7 +9,7 @@ warnings.filterwarnings("ignore")
 lp = pandas.read_csv('/content/TFG-Martinez/GDP_211P_NOC_NB_A-full-2023-10-18.csv')
 
 lp = lp[['ref_area.label','time','obs_value']]
-lp.rename(columns={'ref_area.label':'ADMIN','obs_value':'Output per worker (GDP constant 2017 international $ at PPP)'},inplace=True)
+lp.rename(columns={'ref_area.label':'ADMIN','obs_value':'LP'},inplace=True)
 lp1 = lp[['ADMIN','Trade union density rate (%)']]
 lp1 = lp1.groupby(['ADMIN']).mean().reset_index()
 lp1 = lp1.fillna(0)
@@ -19,7 +19,7 @@ world = geopandas.read_file('/content/TFG-Martinez/ne_110m_admin_0_countries.zip
 mydata = lp1.merge(world,on='ADMIN',how='right')
 mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
 fig,ax = plt.subplots(1,figsize=(50,50))
-mydata.plot(column='Trade union density rate (%)',alpha=0.8,cmap='viridis',ax=ax,legend=True,legend_kwds={'shrink':0.3})
-ax.set_title('Trade Union Density Rate (%)\nSource: International Labour Organization')
+mydata.plot(column='LP',alpha=0.8,cmap='viridis',ax=ax,legend=True,legend_kwds={'shrink':0.3})
+ax.set_title('Output per worker (GDP constant 2017 international $ at PPP)\nSource: International Labour Organization')
 ax.axis('off')
 fig.savefig('/content/TFG-Martinez/World.png')
