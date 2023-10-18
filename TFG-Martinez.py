be6ea11790f01663d6549b11cc608ab45f7dd192
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-18 14:21:30 +0200

    Update TFG-Martinez.py

--- TFG-Martinez.py
+++ TFG-Martinez.py
@@ -16,8 +16,10 @@ lp1 = lp1.fillna(0)
 
 world = geopandas.read_file('/content/TFG-Martinez/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
 world.loc[world.ADMIN=='United States of America','ADMIN'] = 'United States'
+
 mydata = lp1.merge(world,on='ADMIN',how='right')
 mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
+
 fig,ax = plt.subplots(1,figsize=(50,50))
 mydata.plot(column='LP',alpha=0.8,cmap='viridis',ax=ax,legend=True,legend_kwds={'shrink':0.3})
 ax.set_title('Output per worker (GDP constant 2017 international $ at PPP)\nSource: International Labour Organization')
