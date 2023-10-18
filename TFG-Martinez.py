1e79d974e4d77fcf691fec22495d1b03ee0790fc
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-18 13:46:13 +0200

    Update TFG-Martinez.py

--- TFG-Martinez.py
+++ TFG-Martinez.py
@@ -10,12 +10,13 @@ ilo = pandas.read_csv('/content/TFG-Martinez/ILR_TUMT_NOC_RT_A-filtered-2023-10-
 
 ilo = ilo[['ref_area.label','time','obs_value']]
 ilo.rename(columns={'ref_area.label':'ADMIN','obs_value':'Trade union density rate (%)'},inplace=True)
-ilo2019 = ilo[ilo.time==2019]
-ilo2019 = ilo2019.fillna(0)
+ilo1 = ilo[['ADMIN','Trade union density rate (%)']]
+ilo1 = ilo1.groupby(['ADMIN']).mean().reset_index()
+ilo1 = ilo1.fillna(0)
 
 world = geopandas.read_file('/content/TFG-Martinez/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
 
-mydata = ilo2019.merge(world,on='ADMIN',how='right')
+mydata = ilo1.merge(world,on='ADMIN',how='right')
 mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
 fig,ax = plt.subplots(1,figsize=(20,20))
 mydata.plot(column='Trade union density rate (%)',alpha=0.8,cmap='viridis',ax=ax,legend=True)
