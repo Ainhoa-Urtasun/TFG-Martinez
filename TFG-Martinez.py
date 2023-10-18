0269f705a6c5706a991d9863cbeef86524416d2d
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-18 14:13:36 +0200

    Update TFG-Martinez.py

--- TFG-Martinez.py
+++ TFG-Martinez.py
@@ -6,17 +6,17 @@ import pyproj
 import warnings
 warnings.filterwarnings("ignore")
 
-ilo = pandas.read_csv('/content/TFG-Martinez/ILR_TUMT_NOC_RT_A-filtered-2023-10-18.csv')
+lp = pandas.read_csv('/content/TFG-Martinez/GDP_211P_NOC_NB_A-full-2023-10-18.csv')
 
-ilo = ilo[['ref_area.label','time','obs_value']]
-ilo.rename(columns={'ref_area.label':'ADMIN','obs_value':'Trade union density rate (%)'},inplace=True)
-ilo1 = ilo[['ADMIN','Trade union density rate (%)']]
-ilo1 = ilo1.groupby(['ADMIN']).mean().reset_index()
-ilo1 = ilo1.fillna(0)
+lp = lp[['ref_area.label','time','obs_value']]
+lp.rename(columns={'ref_area.label':'ADMIN','obs_value':'Output per worker (GDP constant 2017 international $ at PPP)'},inplace=True)
+lp1 = lp[['ADMIN','Trade union density rate (%)']]
+lp1 = lp1.groupby(['ADMIN']).mean().reset_index()
+lp1 = lp1.fillna(0)
 
 world = geopandas.read_file('/content/TFG-Martinez/ne_110m_admin_0_countries.zip')[['ADMIN','geometry']]
 
-mydata = ilo1.merge(world,on='ADMIN',how='right')
+mydata = lp1.merge(world,on='ADMIN',how='right')
 mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
 fig,ax = plt.subplots(1,figsize=(50,50))
 mydata.plot(column='Trade union density rate (%)',alpha=0.8,cmap='viridis',ax=ax,legend=True,legend_kwds={'shrink':0.3})
