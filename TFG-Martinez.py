af72eb0822207f49f02fbe335edd0db45f7fd35d
Ainhoa <ainhoa.urtasun@unavarra.es>
2023-10-18 13:39:28 +0200

    Update TFG-Martinez.py

--- TFG-Martinez.py
+++ TFG-Martinez.py
@@ -15,7 +15,7 @@ world = geopandas.read_file('/content/TFG-Martinez/ne_110m_admin_0_countries.zip
 
 mydata = ilo.merge(world,on='ADMIN',how='right')
 mydata = geopandas.GeoDataFrame(mydata,geometry='geometry')
-fig,ax = plt.subplots(1,figsize=(10,10))
-mydata.plot(column='Trade union density rate (%)',alpha=0.8,cmap='cool',ax=ax,legend=True)
+fig,ax = plt.subplots(1,figsize=(20,20))
+mydata.plot(column='Trade union density rate (%)',alpha=0.8,cmap='viridis',ax=ax,legend=True)
 ax.set_title('Trade Union Density Rate in Percentages\n(source: International Labour Organization)')
 ax.axis('off')
